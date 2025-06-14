e7f148641e90f79315bbe13e65dea97a0babce80
ahmed medhat <am510836@gmail.com>
2025-06-14 06:47:40 +0300

    update: styling and colors

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import Home from "./components/Home";
 
 
 function App() {
-  return ( 
-    <Home/>
+  return (
+    <>
+      <h1 className="text-3xl mb-10">ابوعبير اتهور و داخل يخطط ليومه , عايزك تتشقلب وتتنطط يزعيم</h1> 
+      <Home/>
+    </>
   );
   
 }
