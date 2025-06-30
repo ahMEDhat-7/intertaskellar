095a47e2404965fb09895c3276b8703bc854c2ae
ahMEDhat <44676992+ahMEDhat-7@users.noreply.github.com>
2025-06-30 07:43:33 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Home from "./components/Home";
 function App() {
   return (
     <>
-      <h1 className="text-3xl mb-10">ابوعبير اتهور و داخل يخطط ليومه , عايزك تتشقلب وتتنطط يزعيم</h1> 
+      <h1 className="text-3xl mb-10">Plan Your Day</h1> 
       <Home/>
     </>
   );
